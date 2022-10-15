2d47b310f5e0e451c91faae1b9b4e9255edb2044
Emmanuel Onwuka <peterkhamsyemmanuel@gmail.com>
2022-10-14 21:53:48 -0700

    Writing scripts on C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 gcc = -E 4-main.c
 puts("Programming is like building a multilingual puzzle");
+return (0);
+}
