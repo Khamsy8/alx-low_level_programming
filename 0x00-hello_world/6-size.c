328df5f8488752d9154c45a9e21c9392bbb6a7ed
Emmanuel Onwuka <peterkhamsyemmanuel@gmail.com>
2022-10-16 17:23:45 -0700

    C progamming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,9 @@
  */
 int main(void)
 {
+char charType;
+int intType;
+longint longintType;
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
