59d527fa0d8860a2bedeadc1844e0f8bd32a6134
Emmanuel Onwuka <peterkhamsyemmanuel@gmail.com>
2022-10-19 07:52:29 -0700

    more codes

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -17,18 +17,9 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n > 5)
-{
-printf("Last digit of 98 is 8 and is greater than 5\n");
-}
-if (n == 0)
-{
-printf("Last digit of 980 is 0 and is 0\n");
-}
-
 if (n < 6 && n != 0)
 {
-printf("Last digit of -98 is -8 and is less than 6 and not 0\n");
+putchar("abcdefghijklmnopqrstuvwxyz\n");
 }
 return (0);
 }
