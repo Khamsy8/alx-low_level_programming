56cffeee799d65605d1d327429e6dd232e2c956a
Emmanuel Onwuka <peterkhamsyemmanuel@gmail.com>
2022-10-19 07:48:36 -0700

    more codes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: "Program notice of the code"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 5)
+{
+printf("Last digit of 98 is 8 and is greater than 5\n");
+}
+if (n == 0)
+{
+printf("Last digit of 980 is 0 and is 0\n");
+}
+
+if (n < 6 && n != 0)
+{
+printf("Last digit of -98 is -8 and is less than 6 and not 0\n");
+}
+return (0);
+}
